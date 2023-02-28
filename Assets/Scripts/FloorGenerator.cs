608c06785db244d7d4f6664de17f55f23c2d8896
maropiyo <maropiyooo@gmail.com>
2023-02-28 20:45:30 +0900

    床を自動生成するスクリプトを作成

---
+++ Assets/Scripts/FloorGenerator.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class GenerateFloor : MonoBehaviour
+{
+    public GameObject floorPrefab1; // 生成する床のプレハブ1
+    public GameObject floorPrefab2; // 生成する床のプレハブ2
+    public float floorGap = 3.0f; // 床のY軸方向の間隔
+    public float minX = 0.0f; // 床の最小X座標
+    public float maxX = 5.0f; // 床の最大X座標
+    public int startFloorCount = 10; // 最初に生成する床の数
+    public float startFloorY = 0.0f; // 最初に生成する床の位置のY座標
+
+    private float playerHeight; // プレイヤーの高さ
+    private float lastFloorY; // 最後に生成した床のY座標
+    private List<GameObject> floors = new List<GameObject>(); // 生成した床のリスト
+
+    private void Start()
+    {
+        lastFloorY = startFloorY;
+        for (int i = 0; i < startFloorCount; i++)
+        {
+            GenerateNextFloor();
+        }
+    }
+
+    private void Update()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        playerHeight = player.transform.position.y;
+        if (playerHeight > lastFloorY + floorGap)
+        {
+            GenerateNextFloor();
+        }
+        RemoveFloor();
+    }
+
+    private void GenerateNextFloor()
+    {
+        float randomX = Random.Range(minX, maxX);
+        GameObject floorPrefab = Random.value > 0.5f ? floorPrefab1 : floorPrefab2;
+        GameObject newFloor = Instantiate(floorPrefab, new Vector3(randomX, lastFloorY + floorGap, 0), Quaternion.identity);
+        floors.Add(newFloor);
+        lastFloorY = newFloor.transform.position.y;
+    }
+
+    private void RemoveFloor()
+    {
+        for (int i = 0; i < floors.Count; i++)
+        {
+            if (floors[i].transform.position.y < playerHeight - 10.0f)
+            {
+                Destroy(floors[i]);
+                floors.RemoveAt(i);
+                i--;
+            }
+        }
+    }
+}
+
