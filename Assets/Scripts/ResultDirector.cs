4516a42a8dcf30d31ebb2f9e4211ab94350ac768
maropiyo <maropiyooo@gmail.com>
2023-03-08 00:07:00 +0900

    リザルト画面にハイスコアを表示する処理を追加

--- Assets/Scripts/ResultDirector.cs
+++ Assets/Scripts/ResultDirector.cs
@@ -6,10 +6,20 @@ public class ResultDirector : MonoBehaviour
 {
     // リザルトスコアのテキスト
     public TextMeshProUGUI resultScoreText;
+    // ハイスコアのテキスト
+    public TextMeshProUGUI resultHighScoreText;
+
+    // ハイスコア
+    private int highScore;
 
     private void Start()
     {
+        // スコアを表示
         resultScoreText.text = $"Score\n{GameDirector.score}";
+
+        // ハイスコアを取得して表示
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
+        resultHighScoreText.text = $"High: {highScore}";
     }
 
     // リスタートボタンが押された時
