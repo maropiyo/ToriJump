75bd82c07053a593be65adaa11cdf26f7058afa6
maropiyo <maropiyooo@gmail.com>
2023-03-07 23:28:45 +0900

    リザルト画面にスコアを表示

--- Assets/Scripts/ResultDirector.cs
+++ Assets/Scripts/ResultDirector.cs
@@ -1,8 +1,17 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ResultDirector : MonoBehaviour
 {
+    // リザルトスコアのテキスト
+    public TextMeshProUGUI resultScoreText;
+
+    private void Start()
+    {
+        resultScoreText.text = $"Score\n{GameDirector.score}";
+    }
+
     // リスタートボタンが押された時
     public void OnRestartButtonClicked()
     {
