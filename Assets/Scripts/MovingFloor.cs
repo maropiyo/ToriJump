fb667be4e280fdaaf75163fe35f0346109a379c9
maropiyo <maropiyooo@gmail.com>
2023-03-13 00:11:34 +0900

    動く床のプレファブを追加

---
+++ Assets/Scripts/MovingFloor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class MovingFloor : MonoBehaviour
+{
+    public float speed = 2f; // 移動速度
+    public float leftLimit = 0f; // 左端の座標
+    public float rightLimit = 5f; // 右端の座標
+
+    private bool moveRight = true;
+
+    void Update()
+    {
+        if (transform.position.x > rightLimit)
+        {
+            moveRight = false;
+        }
+        else if (transform.position.x < leftLimit)
+        {
+            moveRight = true;
+        }
+
+        if (moveRight)
+        {
+            transform.position = new Vector2(transform.position.x + speed * Time.deltaTime, transform.position.y);
+        }
+        else
+        {
+            transform.position = new Vector2(transform.position.x - speed * Time.deltaTime, transform.position.y);
+        }
+    }
+}
