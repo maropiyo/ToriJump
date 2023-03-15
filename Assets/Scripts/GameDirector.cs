dfc05859798cb6ce94573ff7e3cc9464ab1d72ef
maropiyo <maropiyooo@gmail.com>
2023-03-15 21:14:31 +0900

    変数名とコメントの修正

--- Assets/Scripts/GameDirector.cs
+++ Assets/Scripts/GameDirector.cs
@@ -48,20 +48,20 @@ public class GameDirector : MonoBehaviour
 
     private void CheckPlayerPosition()
     {
-        // ワールド座標をビューポート座標に変換する
-        Vector3 viewportPosition = mainCamera.WorldToViewportPoint(player.position);
+        // プレイヤーのワールド座標をビューポート座標に変換する
+        Vector3 playerViewportPosition = mainCamera.WorldToViewportPoint(player.position);
 
-        // ビューポート座標が画面外にある場合
-        if (viewportPosition.x < 0f || 1f < viewportPosition.x)
+        // プレイヤーがメインカメラの範囲外（左右）に行った場合
+        if (playerViewportPosition.x < 0f || 1f < playerViewportPosition.x)
         {
             // 反対側にワープする
             Vector3 newPosition = player.position;
 
-            if (viewportPosition.x < 0f)
+            if (playerViewportPosition.x < 0f)
             {
                 newPosition.x = mainCamera.ViewportToWorldPoint(new Vector3(1f, 0f, 0f)).x;
             }
-            else if (viewportPosition.x > 1f)
+            else if (playerViewportPosition.x > 1f)
             {
                 newPosition.x = mainCamera.ViewportToWorldPoint(new Vector3(0f, 0f, 0f)).x;
             }
